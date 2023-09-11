4319f15fca7f648598ed1977e37b0dd050951320
fyosua <74283479+fyosua@users.noreply.github.com>
2023-09-11 06:47:59 +0000

    push 3

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+    "vercel-deploy": {
+      enabled: true,
+      config: {
+        deployHook:
+          "https://api.vercel.com/v1/integrations/deploy/prj_YCj2pbQfQLWfguxrgxSFmWQedtmB/TXIUeHXOls",
+        apiToken: "IPg0nk50XtEGNfJ5xBs86G7y",
+        appFilter: "strapi-test-fntdjisv1-fyosua.vercel.app",
+        // teamFilter: "your-team-id-on-vercel",
+        roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+      },
+    },
+  });
