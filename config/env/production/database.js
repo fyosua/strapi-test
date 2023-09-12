db3b2356fe1236af7c6dde25cdb954aad7b775e8
fyosua <74283479+fyosua@users.noreply.github.com>
2023-09-12 08:48:10 +0000

    update DB

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,20 +1,44 @@
 const { parse } = require("pg-connection-string");
+const path = require('path');
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  const client = env('DATABASE_CLIENT', 'sqlite');
+  if(client != "sqlite"){
+    const { host, port, database, user, password } = parse(env("DATABASE_URL"));
   
+    return {
+      connection: {
+        client: 'postgres',
+        connection: {
+          host,
+          port,
+          database,
+          user,
+          password,
+          ssl: { rejectUnauthorized: false },
+        },
+        debug: false,
+      },
+    }
+  }
+  const connections = {
+    sqlite: {
+      connection: {
+        filename: path.join(
+          __dirname,
+          '..',
+          env('DATABASE_FILENAME', '.tmp/data.db')
+        ),
+      },
+      useNullAsDefault: true,
+    },
+  };
+
   return {
     connection: {
-      client: 'postgres',
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: { rejectUnauthorized: false },
-      },
-      debug: false,
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
     },
-  }
+  };
 };
