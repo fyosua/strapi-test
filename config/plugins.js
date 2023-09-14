7c02010abd346029cd440320991b51286d0f533e
fyosua <74283479+fyosua@users.noreply.github.com>
2023-09-14 05:02:03 +0000

    update db schema

--- config/plugins.js
+++ config/plugins.js
@@ -24,7 +24,7 @@ module.exports = ({ env }) => ({
         depthLimit: 7,
         amountLimit: 100,
         apolloServer: {
-          tracing: false,
+          tracing: true,
         },
       },
     },
